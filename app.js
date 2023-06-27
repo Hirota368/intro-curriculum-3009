20d83034e4ef9571a97c4adb9843029beb665eee
Hirota <ta-hirota@kddi.com>
2023-06-27 14:18:04 +0900

    async/awaitを使った解答

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 'use strict';
-const fs = require('node:fs');
+const fs = require('node:fs').promises;
 
-function cat(fileName) {
-  const content = fs.readFile(fileName, 'utf8', () => {});
+async function cat(fileName) {
+  const content = await fs.readFile(fileName, 'utf8');
   console.log(content);
 }
+
+console.log(process.argv[2]);
 
 cat(process.argv[2]);
